213fef4e2134be45b4d1b095a2f408d87aa60f33
Ganesh Kakade <17514413+ganeshkakade@users.noreply.github.com>
2022-09-29 22:30:15 +0530

    add opencv method for image conversion

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -1,10 +1,15 @@
 from PIL import Image
+import cv2
 
 def convert_png_to_jpg():
-    print("converting png to jpg...")
     img_png = Image.open('path_to_image_file.png')
     img_png.save('modified_path_to_image_file.jpg')
 
+def convert_png_to_jpg_opencv():
+    png_img = cv2.imread('path_to_image_file.png')
+    cv2.imwrite('modified_path_to_image_file.jpg', png_img, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
 if __name__ == '__main__':
+    print("converting png to jpg...")
     convert_png_to_jpg()
 
